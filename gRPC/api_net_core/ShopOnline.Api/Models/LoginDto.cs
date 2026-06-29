67cb4c8d9e99646b150068a5f4e3df41f1cfeb88
DESKTOP-O2K9CPE\chenh <chenhchihuyckc@gmail.com>
2026-06-29 16:05:08 +0700

    Commit code.

---
+++ gRPC/api_net_core/ShopOnline.Api/Models/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopOnline.Api.Models
+{
+    public class LoginDto
+    {
+        public string UserName { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
---
+++ gRPC/api_net_core/ShopOnline.Common/Messages/CategoryCreatedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShopOnline.Common.Messages
+{
+    // The message data that will be sent through RabbitMQ
+    public class CategoryCreatedEvent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
